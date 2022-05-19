41b069b5473ca96411742cc3b725e2155b08ec62
Illyra <58012807+Illyra@users.noreply.github.com>
2022-05-18 18:26:47 -0600

    Delete Homework2 directory

--- Homework2/includes/dbh.inc.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$dbServername = "localhost";
-$dbUsername = "root";
-$dbPassword = "";
-$dbName = "hemeworkl2part2_cars";
-
-$conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
